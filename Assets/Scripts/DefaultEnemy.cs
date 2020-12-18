b6c7e9e26636369a295af45dde85c852cf32039b
Skunkynator <skumac.lukas@gmail.com>
2020-12-18 23:04:29 +0100

    First Enemy Movement Logic

--- Assets/Scripts/DefaultEnemy.cs
+++ Assets/Scripts/DefaultEnemy.cs
@@ -1,22 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class DefaultEnemy : Enemy
 {
+    private Rigidbody2D rb;
+    private Collider2D myCollider;
+    [SerializeField]
+    private LayerMask mask;
+    private Vector2 moveDir = Vector2.zero;
+    [SerializeField]
+    private float moveSpeed = 2;
+    UnityAction updateMovement;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+        updateMovement += checkMovement;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        updateMovement?.Invoke();
+    }
+    void checkMovement()
+    {
+        updateMovement -= checkMovement;
+        updateMovement += move;
+        if(CheckForPlayer())
+        {
+            moveDir = CharacterController.instance.transform.position - transform.position;
+            StartCoroutine(waitForCheck(0.1f));
+        }
+        else
+        {
+            moveDir += new Vector2(Random.Range(-1f, 1f),Random.Range(-1f, 1f)) * 0.25f;
+            StartCoroutine(waitForCheck(Random.Range(0.1f, 0.5f)));
+        }
+        moveDir.Normalize();
+    }
+    void move()
+    {
+        rb.position += moveDir * Time.deltaTime * moveSpeed;
+    }
+    IEnumerator waitForCheck(float moveTime)
+    {
+        yield return new WaitForSeconds(moveTime);
+        updateMovement -= move;
+        updateMovement += checkMovement;
     }
     bool CheckForPlayer()
     {
-       return false;  
+        Vector2 direction = CharacterController.instance.transform.position - transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,direction, direction.magnitude , mask);
+        
+        return hit.collider == null;  
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(myCollider.IsTouchingLayers(mask))
+        {
+            Vector2 normal = collision.GetContact(0).normal;
+            moveDir = Vector2.Reflect(moveDir,normal) - moveDir * normal * 0.2f;
+        }
     }
 }
