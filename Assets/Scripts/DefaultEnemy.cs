eae8a0056b12a960c4eaa102af12c26f230bc89c
Skunkynator <skumac.lukas@gmail.com>
2020-12-19 00:30:55 +0100

    Added Enemy Default Attack

--- Assets/Scripts/DefaultEnemy.cs
+++ Assets/Scripts/DefaultEnemy.cs
@@ -10,24 +10,49 @@ public class DefaultEnemy : Enemy
     private LayerMask mask;
     [SerializeField]
     private float moveSpeed = 2;
+    [SerializeField]
+    private float shotsPerSecond = 1;
+    [SerializeField]
+    private Bullet enemyBullet;
 
     private Collider2D myCollider;
     private Rigidbody2D rb;
     private Vector2 moveDir = Vector2.zero;
-    
+    private bool seeingPlayer = false;
+    private float attackCooldown = 0;
+
+    private Vector3 playerPos => CharacterController.instance.transform.position;
+
     UnityAction updateMovement;
-    // Start is called before the first frame update
+    UnityAction updateAttack;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<Collider2D>();
         updateMovement += checkMovement;
     }
-
-    // Update is called once per frame
     void Update()
     {
         updateMovement?.Invoke();
+        updateAttack?.Invoke();
+        attackCooldown -= Time.deltaTime;
+    }
+    void onPlayerSeen()
+    {
+        seeingPlayer = true;
+        updateAttack += AttackPlayer;
+    }
+    void onPlayerHidden()
+    {
+        seeingPlayer = false;
+        updateAttack -= AttackPlayer;
+    }
+    void AttackPlayer()
+    {
+        if(attackCooldown > 0) return;
+        Instantiate(enemyBullet).init(playerPos - transform.position, true, transform.position);
+        attackCooldown = 1 / shotsPerSecond;
     }
     void checkMovement()
     {
@@ -35,13 +60,15 @@ public class DefaultEnemy : Enemy
         updateMovement += move;
         if(CheckForPlayer())
         {
-            moveDir = CharacterController.instance.transform.position - transform.position;
+            moveDir = playerPos - transform.position;
             StartCoroutine(waitForCheck(0.1f));
+            if(!seeingPlayer) onPlayerSeen();
         }
         else
         {
             moveDir += new Vector2(Random.Range(-1f, 1f),Random.Range(-1f, 1f)) * 0.25f;
             StartCoroutine(waitForCheck(Random.Range(0.1f, 0.5f)));
+            if(seeingPlayer) onPlayerHidden();
         }
         moveDir.Normalize();
     }
@@ -49,19 +76,22 @@ public class DefaultEnemy : Enemy
     {
         rb.position += moveDir * Time.deltaTime * moveSpeed;
     }
+
+    bool CheckForPlayer()
+    {
+        Vector2 direction = playerPos - transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,direction, direction.magnitude , mask);
+        
+        return hit.collider == null;  
+    }
+    
     IEnumerator waitForCheck(float moveTime)
     {
         yield return new WaitForSeconds(moveTime);
         updateMovement -= move;
         updateMovement += checkMovement;
     }
-    bool CheckForPlayer()
-    {
-        Vector2 direction = CharacterController.instance.transform.position - transform.position;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position,direction, direction.magnitude , mask);
-        
-        return hit.collider == null;  
-    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(myCollider.IsTouchingLayers(mask))
