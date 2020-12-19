a2b3a3c30a4a9bc2ac6db973e13638fc3aa2a8a8
Skunkynator <skumac.lukas@gmail.com>
2020-12-19 11:18:20 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/Skunkynator/JingleHell""
    This reverts commit 25f6b2bae1c353f1336f0ed936acef2c688933ce.

--- Assets/Scripts/GameMasterScript.cs
+++ Assets/Scripts/GameMasterScript.cs
@@ -1,23 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameMasterScript : MonoBehaviour
 {
 	[SerializeField]
 	GameObject player;
+	[SerializeField]
 	CharacterController playerCharacterController;
-
+	[SerializeField]
+	GameObject gameOverUI;
 
 	void Awake()
 	{
-		playerCharacterController = player.GetComponent<CharacterController>();
+
+		Time.timeScale = 1;
 	}
 
-	public void gameOver()
+	public void GameOver()
 	{
 		Debug.Log("Game Over");
+		gameOverUI.SetActive(true);
 		Time.timeScale = 0;
 		Destroy(player);
+	}
+	
+	public void ReloadCurrentLevel()
+	{
+		Time.timeScale = 1;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void LoadLevel(int buildIndex)
+	{
+		SceneManager.LoadScene(buildIndex);
+	}
+
+	public void LoadMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void TogglePauseGame()
+	{
+
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
 	}
 }
