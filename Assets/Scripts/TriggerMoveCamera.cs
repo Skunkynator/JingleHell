c8bcc2fe98c4cc0a43fbf77789a2fd7057cd14d8
Benedikt.W <wernig.benedikt@gmail.com>
2020-12-19 07:49:53 +0100

    Added Room Prefab V0.1

---
+++ Assets/Scripts/TriggerMoveCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMoveCamera : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public Camera camera;
+	public Transform cameraTargetPosition;
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		Debug.Log(collider.gameObject.tag);
+		if (collider.gameObject.tag == "Player")
+		{
+			camera.transform.position = cameraTargetPosition.position;
+		}
+	}
+	
+}
